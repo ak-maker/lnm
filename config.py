eb4e6ec76cd84bd6c338e10450b4624272c90f92
Izsak <64005655+ak-maker@users.noreply.github.com>
2024-02-12 15:56:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ CONFIG_PATH = Path("./.env")
 # TODO: Refactor ENV variables with SGPT_ prefix.
 DEFAULT_CONFIG = {
     "model": os.getenv("MODEL", "gpt4-1106-preview"),
-    "base_url": os.getenv("OPENAI_BASE_URL", "https://drchat.xyz"),
-    "api_key": os.getenv("OPENAI_API_KEY", "sk-AQskrHBcKQ1k0sR5vUmwDQ")
+    "base_url": os.getenv("OPENAI_BASE_URL", the_url),
+    "api_key": os.getenv("OPENAI_API_KEY", your_key)
     # New features might add their own config variables here.
 }
 
